4f061c214b32fb4c921e8929048e15c10886ca9d
tcar <tihomir.car@uniqcast.com>
2018-04-09 21:21:02 +0200

    hanoi-solver: seting up webpack config and react project;
    Signed-off-by: tcar <tihomir.car@uniqcast.com>

---
+++ hanoi-solver/src/components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+
+class App extends Component 
+{
+
+    render()
+    {
+        return(
+            <div>
+            <h1>tower of hanoi solver</h1>
+        </div>
+        )
+
+    }
+}
+
+export default App
